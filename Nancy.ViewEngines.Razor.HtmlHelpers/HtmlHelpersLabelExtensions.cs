fddcb71b49ee4362e5b91aac2634af3030f715f4
David Whitney <david@davidwhitney.co.uk>
2014-08-01 09:47:56 +0100

    Added basic port of label extensions

---
+++ Nancy.ViewEngines.Razor.HtmlHelpers/HtmlHelpersLabelExtensions.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using Nancy.Helpers;
+
+namespace Nancy.ViewEngines.Razor.HtmlHelpers
+{
+    public static class HtmlHelpersLabelExtensions
+    {
+        public static IHtmlString Label<TModel>(this HtmlHelpers<TModel> helper, string labelText)
+        {
+            return Label(helper, labelText, null, null);
+        }
+
+        public static IHtmlString Label<TModel>(this HtmlHelpers<TModel> helper, string labelText, string labelFor)
+        {
+            return Label(helper, labelText, labelFor, null);
+        }
+
+        public static IHtmlString Label<TModel>(this HtmlHelpers<TModel> helper, string labelText, object attributes)
+        {
+            return Label(helper, labelText, null, TypeHelper.ObjectToDictionary(attributes));
+        }
+
+        public static IHtmlString Label<TModel>(this HtmlHelpers<TModel> helper, string labelText, string labelFor, object attributes)
+        {
+            return Label(helper, labelText, labelFor, TypeHelper.ObjectToDictionary(attributes));
+        }
+
+        public static IHtmlString Label<TModel>(this HtmlHelpers<TModel> helper, string labelText, string labelFor, IDictionary<string, object> attributes)
+        {
+            if (String.IsNullOrEmpty(labelText))
+            {
+                throw new ArgumentException("Argument_Cannot_Be_Null_Or_Empty", "labelText");
+            }
+
+            labelFor = labelFor ?? labelText;
+
+            var tag = new TagBuilder("label")
+            {
+                InnerHtml = String.IsNullOrEmpty(labelText) ? String.Empty : HttpUtility.HtmlEncode(labelText)
+            };
+
+            if (!String.IsNullOrEmpty(labelFor))
+            {
+                tag.MergeAttribute("for", labelFor);
+            }
+            tag.MergeAttributes(attributes, false);
+
+            return tag.ToHtmlString(TagRenderMode.Normal);
+        }
+    }
+}
