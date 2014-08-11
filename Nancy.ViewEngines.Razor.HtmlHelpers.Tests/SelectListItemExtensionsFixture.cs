fe614a645b98b1fa6db76ca2469c37a46e406a92
David Whitney <david@davidwhitney.co.uk>
2014-08-11 17:47:10 +0100

    adding test project

---
+++ Nancy.ViewEngines.Razor.HtmlHelpers.Tests/SelectListItemExtensionsFixture.cs
@@ -0,0 +1,34 @@
+﻿namespace Nancy.ViewEngines.Razor.HtmlHelpers.Tests
+{
+    using Xunit;
+    using System.Linq;
+
+    public class SelectListItemExtensionsFixture
+    {
+        private TestEnum field;
+
+        [Fact]
+        public void Should_create_list_items_from_enum()
+        {
+            var items = field.ToSelectListItems().ToList();
+            
+            Assert.Equal(items[0].Text, "One");
+            Assert.Equal(items[1].Text, "Two");
+            Assert.Equal(items[2].Text, "Three");
+        }
+
+        [Fact]
+        public void Selected_item_should_have_attribute_set()
+        {
+            var items = field.ToSelectListItems(TestEnum.Three).ToList();
+            
+            Assert.Equal(items[2].Text, "Three");
+            Assert.Equal(items[2].Selected, true);
+        }
+
+        public enum TestEnum
+        {
+            One, Two, Three
+        }
+    }
+}
